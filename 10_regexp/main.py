139ea0cca76978e757932880ca5349a70d2a9977
Laureline <>
2022-11-15 01:46:15 +0100

    🔸 RegExp - Regular Expression 	- `re` package to use regexp 	- common reg class method [ compile ] 	- common reg pattern (instance) method [ match, find, findall, search, group ] 	- returned from methods having [ span, match ] attributes 	🔹 Exo: password checker

---
+++ 10_regexp/main.py
@@ -0,0 +1,32 @@
+import re
+
+print('''
+-----------------------------------------------------------------------------------
+				REGEXP - REGULAR EXPRESSION
+-----------------------------------------------------------------------------------
+	🔸 Are made to match patterns to identify its existence in a string
+		- package to import: import re
+
+		- re.compute(<STRING-PATTERN>):
+			create a regular expression instance 
+
+		- <PATTERN>.match(<STRING>, [<START>, [<END>]]):
+			tries to find a match at the beginning of the string if <START> and 
+			<END> were not specified
+
+		- <PATTERN>.search(<STRING>):
+			searches for the very first occurrence in the string / return
+			match instance object with attributes `span` and `match`
+
+		- <PATTERN>.find(...):
+			finds the first occurrence matching pattern / returns value found
+
+-----------------------------------------------------------------------------------
+''')
+
+phrase = 'Hellowwww and welcome amongst us! This is 2022.'
+pattern_year = re.compile('Hello')
+print('Pattern stored in `pattern_year`:', pattern_year)
+
+result = pattern_year.match(phrase)
+print(result)
